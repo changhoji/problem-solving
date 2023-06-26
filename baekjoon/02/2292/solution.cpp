7d3e90dd1e1ae38225abfc584894ceb2b00965c4
changhoji <onkkno1@gmail.com>
2023-06-26 22:28:41 +0900

    Add: solution

--- baekjoon/02/2292/solution.cpp
+++ baekjoon/02/2292/solution.cpp
@@ -5,5 +5,20 @@ int main(){
     int N;
     cin >> N;
 
-    
+    int answer = 0;
+    int accumulate = 1;
+    int cycle = 0;
+
+    while(true){
+        answer++;
+        if(accumulate + cycle >= N){
+            break;
+        }
+        accumulate += cycle;
+        cycle += 6;
+    }
+
+    cout << answer << '\n';
+
+    return 0;
 }
